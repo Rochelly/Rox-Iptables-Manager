a866d5f4a63938239d1de8a3fdd71e5795da7a84
Rochelly <rochelly.fernandes@gmai.com>
2023-03-10 16:44:02 -0300

    Criando  o menu principal

--- intefaceMenu.py
+++ intefaceMenu.py
@@ -1,5 +1,17 @@
 import netifaces
 import menuBase
+import curses
+# Configurando a biblioteca para que se possa navegar pelo menu
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+curses.start_color()
+screen.keypad(True)
+
+# Definuindo as cores
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK) # Cor verde
+curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK) # cor Amarela
+
 
 
 # Analisando as interfaces
@@ -28,18 +40,15 @@ def list_interfaces():
 def menu_interfaces():
     menu_items=list_interfaces()
     option = menuBase.menu_principal("Selecione a Rede que deseja configurar o Firewall", menu_items)
-
-#if option != len(menu_items) - 1:
-#    screen.clear()
-#    screen.addstr(0, 0, f"Interface selecionada: {menu_items[option]}")
-#    screen.addstr(1, 0, f"Endereço de rede: {menu_items[option].split()[1]}")
-#    screen.addstr(2, 0, f"Gateway padrão: {gateway}")
-#    screen.refresh()
-#    screen.getch()
-
-# Clean up the curses library
-#screen.keypad(False)
-#curses.echo()
-#curses.endwin()
+    if option != len(menu_items) - 1:
+        screen.clear()
+        screen.addstr(0, 0, f"Interface selecionada: {menu_items[option]}")
+        screen.addstr(1, 0, f"Endereço de rede: {menu_items[option].split()[1]}")
+        screen.addstr(2, 0, f"Gateway padrão: {gateway}")
+        screen.refresh()
+        screen.getch()
+        screen.keypad(False)
+        curses.echo()
+        curses.endwin()
 
    
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-#import menuBase
+import menuBase
 import intefaceMenu
 
-menu_items=intefaceMenu.menu_interfaces()
+
+def main_menu():
+    main_title='Qual Tabela você deseja configurar ?'
+    opcoes=['Input',
+            'Forward - (Para Gateways)',
+            'NAT- (Mascaramentos)']
+    opcoes = ["Olá", "Como vai você?", "Qual é o seu nome?", "Qual é a hora?", "Sair"]
+    menuBase.menu_principal(main_menu,opcoes)
+
+
+main_menu()
+#menu_items=intefaceMenu.menu_interfaces()
+
